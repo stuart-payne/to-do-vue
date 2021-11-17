180e599cb4089689ecf06985daf9ad20ddaedace
stuart-payne <paynestu@gmail.com>
2021-11-17 20:22:10 +0000

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg=""/> -->
+    <ToDoItem label="Random Item" :deadline="new Date(Date.now())" :removeCallback="log"></ToDoItem>
+    <div>
+      <to-do-item v-for="{ id, label, deadline } in mockData" :key="id" :label="label" :deadline="deadline" :removeCallback="log"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoItem
+  },
+  data: () => {
+    return {
+      mockData: [
+        { id: 0, label: "Hi", deadline: new Date(Date.now())},
+        { id: 1, label: "Hi", deadline: new Date(Date.now())},
+        { id: 2, label: "Hi", deadline: new Date(Date.now())},
+        { id: 3, label: "Hi", deadline: new Date(Date.now())}
+      ]
+    }
+  },
+  methods: {
+    log: () => console.log("Hi")
   }
 }
 </script>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+       <h3>{{ label }}</h3>
+       <span>{{ timeLeft }}</span>
+       <button v-on:click="removeCallback">Complete</button>
+    </div>
+</template>
+
+<script>
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime)
+
+export default {
+    name: "ToDoItem",
+    props: {
+        label: String,
+        deadline: Date,
+        removeCallback: Function,
+    },
+    data: () => {
+        return { 
+            isComplete: false,
+        }
+    },
+    computed: {
+        timeLeft(){ 
+            return dayjs(this.deadline).fromNow()
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
